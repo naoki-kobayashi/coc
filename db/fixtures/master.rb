82667278fb0689665de75b909648cdc487cdde47
naoki kobayashi <vagrant@vagrant-centos64.vagrantup.com>
2015-04-28 02:51:43 +0000

    $B%F%9%H%G!<%?:n@.(B

--- db/fixtures/master.rb
+++ db/fixtures/master.rb
@@ -1,3 +1,25 @@
+Video.seed do |s|
+  s.id = 1
+  s.url = 'https://www.youtube.com/embed/hDNlyRRNTFg'
+  s.video_type = 'battle'
+end
+
+VideoTag.seed do |s|
+  s.id = 1 
+  s.video_id = 1
+  s.tag = 'TH9'
+end
+VideoTag.seed do |s|
+  s.id = 2 
+  s.video_id = 1
+  s.tag = '援軍後処理'
+end
+VideoTag.seed do |s|
+  s.id = 3 
+  s.video_id = 1
+  s.tag = '裏ホグ'
+end
+
 TagMaster.seed do |s|
   s.id = 1
   s.tag = 'TH10'
@@ -11,6 +33,22 @@ TagMaster.seed do |s|
   s.tag = 'TH8'
 end
 TagMaster.seed do |s|
-  s.id = 3
-  s.tag = ''
+  s.id = 4
+  s.tag = '援軍後処理'
+end
+TagMaster.seed do |s|
+  s.id = 5
+  s.tag = '援軍先処理'
+end
+TagMaster.seed do |s|
+  s.id = 6
+  s.tag = '裏バル'
+end
+TagMaster.seed do |s|
+  s.id = 7
+  s.tag = '裏ホグ'
+end
+TagMaster.seed do |s|
+  s.id = 8
+  s.tag = 'ジャンプ'
 end
